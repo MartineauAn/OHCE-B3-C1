80c636c7bfda58d61415f8c5c7956b68a0600eeb
martineauAn <martineau.antoine2001@gmail.com>
2023-01-28 13:59:44 +0100

    palindrome test part. 1

--- OHCE.Test/UnitTest1.cs
+++ OHCE.Test/UnitTest1.cs
@@ -4,13 +4,32 @@ namespace OHCE.Test
     {
 
         [Fact(DisplayName = "QUAND on envoie un mot 'laval' ALORS on obtient son miroir")]
-        public void TestMiroirToto()
+        public void TestMiroir()
         {
             //QUAND on envoie un mot
             var resultat = new OHCE().Traitement("laval");
 
             //ALORS on obtient son miroir
             Assert.Contains("laval", resultat);
+        }
+
+        [Fact(DisplayName = "QUAND on envoie un palindrome ALORS on obtient celui-ci ET 'Bien dit !' est ajouté")]
+        public void TestPalindrome()
+        {
+            const string palindrome = "bob";
+
+            //QUAND on envoie un mot
+            var resultat = new OHCE().Traitement(palindrome);
+
+            //ALORS on obtient celui-ci
+            Assert.Contains(palindrome, resultat);
+
+            var indexOfPalindrome = resultat.IndexOf(palindrome, StringComparison.Ordinal);
+            var endOfPalindrome = indexOfPalindrome + palindrome.Length;
+            resultat = resultat[endOfPalindrome..];
+
+            //ET 'Bien dit !' est ajouté
+            Assert.Contains("Bien dit !", resultat);
         }
 
     }
