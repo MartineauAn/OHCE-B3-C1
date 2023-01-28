0db0a99de3d153076e533fcebd5dcfdd0c87e362
martineauAn <martineau.antoine2001@gmail.com>
2023-01-28 13:54:54 +0100

    miroir test part. 1

--- OHCE.Test/UnitTest1.cs
+++ OHCE.Test/UnitTest1.cs
@@ -2,6 +2,16 @@ namespace OHCE.Test
 {
     public class UnitTest1
     {
-        
+
+        [Fact(DisplayName = "QUAND on envoie un mot 'laval' ALORS on obtient son miroir")]
+        public void TestMiroirToto()
+        {
+            //QUAND on envoie un mot
+            var resultat = new OHCE().Traitement("laval");
+
+            //ALORS on obtient son miroir
+            Assert.Contains("laval", resultat);
+        }
+
     }
 }
